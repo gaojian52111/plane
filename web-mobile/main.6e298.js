4e4b5cafb91368ac5d0ab27be5bf0e473b118af9
gaojian52111 <gaojian_up@163.com>
2019-06-26 16:58:46 +0800

    副武器火焰的电的攻击力和攻击频率调整

--- web-mobile/main.6e298.js
+++ web-mobile/main.6e298.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.7cc13.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.07035.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.76578.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.6e298.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -200,12 +200,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.f2ed6.js');
+        require('src/settings.4b69e.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.f2ed6.js');
+        require('src/settings.4b69e.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
