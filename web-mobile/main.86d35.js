a1e46f2644dd79333a8dc789737307058ae7330d
gaojian52111 <gaojian_up@163.com>
2019-06-28 11:26:43 +0800

    病毒死亡把减速状态解除

--- web-mobile/main.86d35.js
+++ web-mobile/main.86d35.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.41af9.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.94f9a.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.e7e24.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.86d35.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
