1fe57408693decf9d9069040d84a440c70cb0f32
gaojian52111 <gaojian_up@163.com>
2019-06-28 13:37:19 +0800

    病毒死亡获得金币增加

--- web-mobile/main.2a7d2.js
+++ web-mobile/main.2a7d2.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e12c6.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.daf29.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.4d6a3.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.2a7d2.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
