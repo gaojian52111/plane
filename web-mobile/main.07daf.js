b0016b058ab453a70d9105a83e307da6735a07c4
gaojian52111 <gaojian_up@163.com>
2019-06-28 10:34:27 +0800

    调整减速病毒的技能范围

--- web-mobile/main.07daf.js
+++ web-mobile/main.07daf.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.48a3e.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.41af9.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.076eb.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.07daf.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
